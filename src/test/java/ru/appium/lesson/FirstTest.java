7dc9c16596b29ea2c3e83c4faccac8482d8e9087
rrudakov <rrudakov@protonmail.com>
2018-07-15 13:59:27 +0300

    First test finished

---
+++ src/test/java/ru/appium/lesson/FirstTest.java
@@ -0,0 +1,47 @@
+package ru.appium.lesson;
+
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+/**
+ * First test using appium.
+ */
+public class FirstTest {
+
+    private AppiumDriver<MobileElement> driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "HUAWEI P9 lite");
+        capabilities.setCapability("platformVersion", "7.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "/home/rrudakov/Work/appium-lesson/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("");
+    }
+}
