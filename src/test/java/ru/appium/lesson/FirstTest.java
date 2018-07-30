0c06cf8a0b64f4aa874830f9817cd7074614a716
rrudakov <rrudakov@protonmail.com>
2018-07-30 22:14:16 +0300

    Set portrait orientation before every test

--- src/test/java/ru/appium/lesson/FirstTest.java
+++ src/test/java/ru/appium/lesson/FirstTest.java
@@ -66,6 +66,7 @@ public class FirstTest {
     capabilities.setCapability("app", "/home/rrudakov/Work/appium-lesson/apks/org.wikipedia.apk");
 
     driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    driver.rotate(ScreenOrientation.PORTRAIT);
   }
 
   @After
